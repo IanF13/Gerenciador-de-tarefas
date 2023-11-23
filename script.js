54efd2799268fa5c675f66457f244eaa3c5f7d2d
IanF13 <130380548+IanF13@users.noreply.github.com>
2023-11-23 17:51:01 -0300

    Adição dos créditos e modificações parte 2

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//Função de editar 
+//Função de adicionar tarefa
+
 function adicionarTarefa() {
     var inputTarefa = document.getElementById('tarefa');
     var tarefa = inputTarefa.value.trim();
@@ -6,37 +9,35 @@ function adicionarTarefa() {
         var ulTarefasPendentes = document.getElementById('tarefas-pendentes');
         var li = document.createElement('li');
 
-        // Adicionei uma div para envolver o texto da tarefa e o botão de editar
+        
         var divConteudo = document.createElement('div');
         divConteudo.appendChild(document.createTextNode(tarefa));
 
-        // Adicionei um botão de edição dentro do item da lista
+        
         var btnEditar = document.createElement('button');
         btnEditar.textContent = 'Editar';
-        btnEditar.classList.add('edit-button'); // Adicionei a classe do botão de editar
-        btnEditar.onclick = function(event) {
-            event.stopPropagation(); // Impedir que a tarefa seja concluída ao clicar no botão de editar
+        btnEditar.classList.add('edit-button'); 
+        btnEditar.onclick = function (event) {
+            event.stopPropagation(); 
             editarTarefa(divConteudo);
         };
-
-        // Estilizei a borda azul e o texto preto
+//Função de completar a tarefa 
+        
         divConteudo.style.border = '2px solid #3498db';
-        divConteudo.style.color = 'black'; // Cor preta para o texto
+        divConteudo.style.color = 'black'; 
         divConteudo.style.padding = '10px';
         divConteudo.style.borderRadius = '5px';
-        divConteudo.style.position = 'relative'; // Permite posicionar o botão de editar de forma absoluta
+        divConteudo.style.position = 'relative'; 
 
-        // Estilizei o botão de editar
         btnEditar.style.position = 'absolute';
-        btnEditar.style.right = '10px'; // Posiciona o botão no canto direito
-        btnEditar.style.top = '50%'; // Centraliza verticalmente
-        btnEditar.style.transform = 'translateY(-50%)'; // Ajusta para centralizar verticalmente
+        btnEditar.style.right = '10px'; 
+        btnEditar.style.top = '50%'; 
+        btnEditar.style.transform = 'translateY(-50%)'; 
 
         divConteudo.appendChild(btnEditar);
         li.appendChild(divConteudo);
 
-        // Adicionei a chamada da função para completarTarefa quando o item for clicado
-        li.onclick = function() {
+        li.onclick = function () {
             completarTarefa(li);
         };
 
@@ -49,58 +50,46 @@ function completarTarefa(tarefa) {
     tarefa.classList.toggle('completed');
     moverTarefa(tarefa);
 
-    // Adicionei a verificação de conclusão para ajustar a cor do texto
     var divConteudo = tarefa.querySelector('div');
-    var btnEditar = tarefa.querySelector('.edit-button'); // Adicionei para encontrar o botão de editar
+    var btnEditar = tarefa.querySelector('.edit-button'); 
 
     if (tarefa.classList.contains('completed')) {
-        divConteudo.style.color = '#2ecc71'; // Cor verde para tarefas concluídas
-        btnEditar.style.borderColor = '#2ecc71'; // Ajusta a borda do botão de editar
+        divConteudo.style.color = '#2ecc71'; 
+        btnEditar.style.borderColor = '#2ecc71'; 
     } else {
-        divConteudo.style.color = 'black'; // Cor preta para tarefas não concluídas
-        btnEditar.style.borderColor = '#3498db'; // Ajusta a borda do botão de editar
+        divConteudo.style.color = 'black'; 
+        btnEditar.style.borderColor = '#3498db'; 
     }
 }
-
-
+//Função de editar conteúdo da tarefa  
 
 function editarTarefa(divConteudo) {
     var novoTexto = prompt('Editar Tarefa:', divConteudo.textContent);
-    
-    // Verifica se o usuário inseriu um novo texto
+
     if (novoTexto !== null) {
         divConteudo.textContent = novoTexto.trim();
+
         
-        // Restaura a classe 'completed' e a borda do botão de editar
         divConteudo.parentElement.classList.remove('completed');
         var btnEditar = divConteudo.querySelector('.edit-button');
         btnEditar.style.borderColor = '#3498db';
     }
 }
-btnEditar.addEventListener('click', function(event) {
-    event.stopPropagation(); // Impedir que o clique no botão propague para o item da lista
-    editarTarefa(divConteudo);
-});
-
-
-
-
-        ulTarefasPendentes.appendChild(li);
-        inputTarefa.value = '';
- 
+//Adicionar tarefa com ENTER
 
 function adicionarComEnter(event) {
     if (event.key === 'Enter') {
         adicionarTarefa();
     }
 }
+//Função de mover a tarefa para concluido
 
 function moverTarefa(tarefa) {
     var ulTarefasConcluidas = document.getElementById('tarefas-concluidas-hoje');
     var ulTarefasPendentes = document.getElementById('tarefas-pendentes');
 
     if (tarefa.classList.contains('completed')) {
-        // Remove estilos específicos da tarefa antes de movê-la
+        
         tarefa.style.border = '';
         tarefa.style.backgroundColor = '';
 
@@ -110,17 +99,25 @@ function moverTarefa(tarefa) {
         ulTarefasPendentes.appendChild(tarefa);
     }
 }
+//Função de limpar tudo
 
-// Adicionei a função para limpar todas as tarefas
 function limparTudo() {
     var ulTarefasPendentes = document.getElementById('tarefas-pendentes');
     var ulTarefasConcluidasHoje = document.getElementById('tarefas-concluidas-hoje');
     var ulTarefasConcluidasSemana = document.getElementById('tarefas-concluidas-semana');
 
+    
     ulTarefasPendentes.innerHTML = '';
     ulTarefasConcluidasHoje.innerHTML = '';
     ulTarefasConcluidasSemana.innerHTML = '';
+
+    
+    var todasAsTarefas = document.querySelectorAll('li');
+    todasAsTarefas.forEach(function (tarefa) {
+        tarefa.style.border = '';
+        tarefa.style.backgroundColor = '';
+    });
 }
 
-// Adicionei a chamada da função para limpar todas as tarefas quando o botão é clicado
+
 document.getElementById('limparTudo').addEventListener('click', limparTudo);
